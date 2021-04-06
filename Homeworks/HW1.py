95a84f61f915d621196dbd6aff87e31b58cdd1f2
OzgurORUC <43278684+OzgurORUC@users.noreply.github.com>
2021-04-06 23:09:22 +0300

    Update HW1.py
    sorular cevaplandı

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,17 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+mylist=[0,1,2,3,4,5,6,7]
+mylist=mylist[4:8]+mylist[0:4]
+print(mylist)
+
+#Question 2 basit cevap
+n=int(input('tek basamaklı bir sayı giriniz:'))
+print(list(range(0,n+1,2)))
+
+#Question 2 diğer cevap
+#while uygulaması ile tek basamaklı sayı girdisine zorluyoruz
+n=10
+while n>9 or n<0:
+  n=int(input('tek basamaklı bir sayı giriniz:'))
+print(list(range(0,n+1,2)))
