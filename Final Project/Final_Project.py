69bd1a95e1fff32de58adea2116bd2eaad383cf2
OzgurORUC <43278684+OzgurORUC@users.noreply.github.com>
2021-04-11 21:04:22 +0300

    Update Final_Project.py

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -1,3 +1,37 @@
-import pandas as pd
+# Final Project
+# Knowledge competition (case sensitive istenmiş ama ben şıklı yaptım)
+sorulist = [{"Türkiye’nin uluslararası otomatik telefon kod numarası kaçtır?":["00","90","80","09"]},
+            {"Pusulada ( N ) harfi hangi yönü ifade eder?":["Güney","Doğu","Batı","Kuzey"]},
+            {"Gece gündüz eşitliği (ekinoks) bir yılda kaç kez gerçekleşir?":["1","2","3","4"]},
+            {"Türkiye’de Erozyonla mücadele amacıyla kurulan vakfın kısa adı nedir?":["TMO","DSİ","Tema","TKK"]},            
+            {"Altın Palmiye sinema ödülü hangi film festivalinde verilmektedir?":["Londra","Paris","Cannes","Barcelona"]},            
+            {"Dünyanın ilk haritasını çizen ünlü Türk denizcisi kimdir?":["İbni Sina","Oruç Reis","Piri Reis","Barbaros Hayrettin"]},
+            {"Çocuk hakları günü hangi tarihte kutlanmaktadır?":["23 Nisan","20 Kasım","28 Eylül","26 Nisan"]},            
+            {"Nobel ödülleri hangi ülkede verilmektedir?":["Almanya","İsveç","Norveç","Danimarka"]},
+            {"Amerika kıtasını 2’ye ayıran önemli su geçidinin adı nedir?":["Süveyş","CebeliTarık","Panama","Berink"]},
+            {"İlk Türk kadın dağcı kimdir?":["İlmiye Bergman","Nasuh MAHRUKİ","Gülnur Tumbat","Burçak Özoğlu Poçan"]}];
+cevaplist=["b","d","b","c","c","c","b","b","c","a"]
+soruno=0
+dogrucevapsay=0
+hatasiklar={}
+hatalist=[]
+for k in sorulist:
+    soruno+=1
+    for s,v in k.items():
+        print("Soru " + str(soruno) + " : ", s, "\n a) ", v[0],"\n b) ", v[1], "\n c) ", v[2],"\n d) ", v[3])    
+        cevap = input("Cevap şıkkını giriniz (a,b,c,d): ")
+        if cevap == cevaplist[soruno-1]:
+            dogrucevapsay+=10
+        else:
+            hatasiklar[soruno]=cevap
+if dogrucevapsay<60:
+    print("Sonucunuz (100 üzerinden): " + str(dogrucevapsay) + ", başarısız oldunuz, tekrar deneyiniz")
+    for k,v in hatasiklar.items():
+           print(str(k) + ".soruyu " + str(v) + " diyerek hatalı cevapladınız, dogru cevap: " + cevaplist[k-1] + "\n")    
+else:
+    print("Sonucunuz (100 üzerinden): " + str(dogrucevapsay) + ", başarılı oldunuz, tebrikler")
+    if dogrucevapsay<100:
+        for k,v in hatasiklar.items():
+           print(str(k) + ".soruyu " + str(v) + " diyerek hatalı cevapladınız, dogru cevap: " + cevaplist[k-1] + "\n")
+            
 
-data = pd.read_csv("project.csv")
